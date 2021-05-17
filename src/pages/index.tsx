ae0c3bb43c59c773ea209c61c7c69caa6b520603
Jefferson Estevam <jesbmail@gmail.com>
2021-05-17 20:53:17 -0300

    started nextTS

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from '../styles/components/Container'
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <h1>Running! TSX</h1>
+    </Container>
+  )
+}
+
+export default Home
+
